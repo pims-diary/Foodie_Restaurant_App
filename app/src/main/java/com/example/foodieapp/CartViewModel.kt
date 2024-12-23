8f786ab33024b2498737883f45f8f8e180269d28
Priyanka Ghosh <speak2priyankaghosh@gmail.com>
2024-12-23 17:43:25 +1300

    Create a basic View Model for cart

---
+++ app/src/main/java/com/example/foodieapp/CartViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.foodieapp
+
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.ViewModel
+
+class CartViewModel : ViewModel() {
+    // List to hold cart items
+    val cartItems = mutableStateListOf<CartItem>()
+
+    // Function to add items to the cart
+    fun addItemToCart(item: CartItem) {
+        cartItems.add(item)
+    }
+
+    // Calculate total cost of items in the cart
+    fun getTotalCost(): Double {
+        return cartItems.sumOf { it.quantity * it.price }
+    }
+}
