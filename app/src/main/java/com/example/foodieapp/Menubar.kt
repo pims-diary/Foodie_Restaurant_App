d7524007d26ccf96d9f5a918633231d0414ed901
Priyanka Ghosh <speak2priyankaghosh@gmail.com>
2025-01-10 19:37:42 +1300

    create contact option in the menu bar using Intent

--- app/src/main/java/com/example/foodieapp/Menubar.kt
+++ app/src/main/java/com/example/foodieapp/Menubar.kt
@@ -1,6 +1,8 @@
 package com.example.foodieapp
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
@@ -10,8 +12,8 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Call
 import androidx.compose.material.icons.outlined.MailOutline
-import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material.icons.outlined.ShoppingCart
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -33,6 +35,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 
@@ -44,6 +47,8 @@ fun MenuBar() {
     val scope = rememberCoroutineScope()
     var currentPage by remember { mutableIntStateOf(0) }
     val cartViewModel = remember { CartViewModel() }
+    val phoneNumber = "+64287422344"
+    val context = LocalContext.current
 
     ModalNavigationDrawer(
         drawerContent = {
@@ -82,13 +87,18 @@ fun MenuBar() {
 
                     HorizontalDivider(modifier = Modifier.padding(vertical = 8.dp))
 
-                    Text("Section 2", modifier = Modifier.padding(16.dp), style = MaterialTheme.typography.titleMedium)
                     NavigationDrawerItem(
-                        label = { Text("Settings") },
+                        label = { Text("Contact") },
                         selected = false,
-                        icon = { Icon(Icons.Outlined.Settings, contentDescription = null) },
-                        badge = { Text("20") }, // Placeholder
-                        onClick = { /* Handle click */ }
+                        icon = { Icon(Icons.Outlined.Call, contentDescription = null) },
+                        onClick = {
+                            // Open the dial pad with the phone number
+                            val intent = Intent(Intent.ACTION_DIAL).apply {
+                                data = Uri.parse("tel:$phoneNumber")
+                            }
+                            context.startActivity(intent)
+
+                        }
                     )
                     NavigationDrawerItem(
                         label = { Text("Help and feedback") },
